b9d668205be5dada1bdc8d0723dbd272131d7ca7
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-06-30 15:40:04 -0500

    paddle control

--- sketch.js
+++ sketch.js
@@ -11,4 +11,26 @@ function draw() {
   background(0);
   playerPaddle.display();
   aiPaddle.display();
+
+  if (playerPaddle.isUp) {
+    playerPaddle.up();
+  } else if (playerPaddle.isDown) {
+    playerPaddle.down();
+  }
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    playerPaddle.isUp = true;
+  } else if (keyCode == DOWN_ARROW) {
+    playerPaddle.isDown = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode == UP_ARROW) {
+    playerPaddle.isUp = false;
+  } else if (keyCode == DOWN_ARROW) {
+    playerPaddle.isDown = false;
+  }
 }
